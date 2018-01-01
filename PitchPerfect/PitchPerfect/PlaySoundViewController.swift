1aa602792b182c648660cfa1deec21f07b80f327
kawnayeen <kawnayeen.08@gmail.com>
2018-01-01 17:12:55 +0600

    All the different audio effect implemented

--- PitchPerfect/PitchPerfect/PlaySoundViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundViewController: UIViewController {
 
-    var recordedAudioURL: URL!
+    
     
     @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
@@ -20,34 +21,48 @@ class PlaySoundViewController: UIViewController {
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
+    var recordedAudioURL: URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("play sound button pressed")
+        
+        switch(ButtonType(rawValue: sender.tag)!){
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.0)
+        case .chipmunk:
+            playSound(pitch: 100)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
-        print("stop audio button pressed")
+        stopAudio()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
-    */
-
 }
