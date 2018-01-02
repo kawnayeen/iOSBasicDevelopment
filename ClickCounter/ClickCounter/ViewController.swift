92fb8ac843c8138f6e08bd3513856d25e2bbace6
kawnayeen <kawnayeen.08@gmail.com>
2018-01-02 15:30:56 +0600

    UI for click counter app developed in interface builder

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -11,27 +11,38 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label : UILabel!
+    @IBOutlet var label : UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Label
+        //addUIElementProgramatically()
+    }
+    
+    func addUIElementProgramatically() {
+        showLabel()
+        showButton()
+    }
+    
+    func showLabel() {
         let label = UILabel()
         label.frame = CGRect(origin: CGPoint(x:175,y:150), size: CGSize(width: 100, height: 30))
         label.text = "0"
         self.view.addSubview(label)
         self.label = label
-        
-        // Button
+    }
+    
+    func showButton() {
         let button = UIButton()
         button.frame = CGRect(origin: CGPoint(x:135, y:200), size: CGSize(width: 100, height: 45))
         button.setTitle("Click Me", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         self.view.addSubview(button)
-        
         // Add button event
         button.addTarget(self, action: #selector(incrementCount), for: UIControlEvents.touchUpInside)
+    }
+    
+    @IBAction func clicked(_ sender: Any) {
+        incrementCount()
     }
     
     func incrementCount() {
