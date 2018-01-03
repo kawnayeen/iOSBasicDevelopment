1670e4cf541f9e3617c56b65da9a2fc33f8adf14
kawnayeen <kawnayeen.08@gmail.com>
2018-01-03 11:07:39 +0600

    ColorMaker app completed

--- ColorMaker/ColorMaker/ViewController.swift
+++ ColorMaker/ColorMaker/ViewController.swift
@@ -10,5 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redControl: UISwitch!
+    @IBOutlet weak var greenControl: UISwitch!
+    @IBOutlet weak var blueControl: UISwitch!
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBAction func changeColorComponent(_ sender: Any) {
+        let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
 }
 
