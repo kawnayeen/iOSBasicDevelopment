10195ba1e998ddcf314793278192fe62a4373fe7
kawnayeen <kawnayeen.08@gmail.com>
2018-01-05 11:28:40 +0600

    TextFieldDelegates project created

---
+++ TextFieldDelegates/TextFieldDelegates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFieldDelegates
+//
+//  Created by Kawnayeen on 1/5/18.
+//  Copyright © 2018 Kawnayeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
