358fd03cba3ad2603ca80f4bfec2aefd0b96bb58
kawnayeen <kawnayeen.08@gmail.com>
2018-01-17 13:47:53 +0600

    bond villian app added

---
+++ BondVillains/BondVillains/VillainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VillainTableViewCell.swift
+//  BondVillains
+//
+//  Created by Kawnayeen on 1/17/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class VillainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
