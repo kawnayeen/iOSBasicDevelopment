439f0af0121b1c6141e0f94c8e764f9e493f2230
kawnayeen <kawnayeen.08@gmail.com>
2018-01-17 12:22:39 +0600

    showing little bit of family history in DoReMi app

--- DoReMi/DoReMi/ViewController.swift
+++ DoReMi/DoReMi/ViewController.swift
@@ -18,11 +18,21 @@ class ViewController: UIViewController, UITableViewDataSource {
     // Storyboard has been set up for you using this String.
     let cellReuseIdentifier = "MyCellReuseIdentifier"
     
+    @IBOutlet weak var myTableView: UITableView!
     // Choose some data to show in your table
     
-    let model: [String] = [
-        // TODO: Fill this array with data
+    let model : [[String:String]] = [
+        ["text": "Munshi Danesh Ali", "detail" : "Father"],
+        ["text": "Azizur Rahman", "detail" : "Son"],
+        ["text": "Badrul Islam", "detail": "Grandson"],
+        ["text": "Kamarul Kawnayeen", "detail": "Great GrandSon"]
     ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myTableView.tableFooterView = UIView()
+        myTableView.estimatedSectionFooterHeight = 0.0
+    }
     
     // MARK: UITableViewDataSource
     
@@ -30,12 +40,14 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //TODO: Implement method to return the correct number of rows.
-        return 0
+        return model.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //TODO: Implement method to return cell with the correct reuseidentifier and populated with the correct data.
-        let placeholderCell = UITableViewCell()
-        return placeholderCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellReuseIdentifier)!
+        let dictionary = self.model[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = dictionary["text"]
+        cell.detailTextLabel?.text = dictionary["detail"]
+        return cell
     }
 }
