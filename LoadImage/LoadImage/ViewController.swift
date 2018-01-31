e8de1ba114f71a5509b3e63db317b08f002c0bd9
kawnayeen <kawnayeen.08@gmail.com>
2018-01-31 16:03:56 +0600

    LoadImage project created

---
+++ LoadImage/LoadImage/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LoadImage
+//
+//  Created by Kawnayeen on 1/31/18.
+//  Copyright © 2018 Kawnayeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
