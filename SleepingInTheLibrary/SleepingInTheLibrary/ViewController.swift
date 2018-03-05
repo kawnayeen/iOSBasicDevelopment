b06ee7876d3ee72ad32335febd7e782ad05d3022
kawnayeen <kawnayeen.08@gmail.com>
2018-03-05 17:03:35 +0600

    fetched image and title showed in ui - sleeping library

--- SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
+++ SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -55,7 +55,35 @@ class ViewController: UIViewController {
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if error == nil {
-                print(data)
+                if let data = data {
+                    let parsedResult: [String:AnyObject]!
+                    do {
+                        parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String : AnyObject]
+                    } catch {
+                        //displayError("Could not parese the data as JSON: '\(data)'")
+                        return
+                    }
+                    //print(parsedResult)
+                    if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject], let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] {
+                        var randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                        randomPhotoIndex = 0
+                        let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
+                        
+                        if let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String, let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String {
+                            let imageURL = URL(string: imageUrlString)
+                            if let imageData = try? Data(contentsOf: imageURL!){
+                                
+                                DispatchQueue.main.async {
+                                    self.photoImageView.image = UIImage(data: imageData)
+                                    self.photoTitleLabel.text = photoTitle
+                                    self.setUI(enabled: true)
+                                }
+                            }
+                            print(imageUrlString)
+                            print(photoTitle)
+                        }
+                    }
+                }
             }
         }
         task.resume()
